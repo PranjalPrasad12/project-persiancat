b0c6b082311218298e419221424dcda3be2da98f
Pranjal Prasad <116650694+PranjalPrasad12@users.noreply.github.com>
2024-12-06 19:13:52 +0530

    Version 0.98-alpha is out!
    Version 0.98-alpha has some new stuff like a interface that is CLI that is actually a little useful. Today, is a special day for the project. The next work is on a Qt or some other toolkit based GUI.
    
    13:43 UTC 6 December 2024

--- persiancat-browser-CLI/main.cpp
+++ persiancat-browser-CLI/main.cpp
@@ -1,3 +1,13 @@
+/*
+  Persiancat Browser CLI- (C) 2024 Pranjal Prasad
+  Project Persiancat's Browser CLI is a 'free' software and you are free to redistribute
+  it under certain conditions of the GPL v3.
+
+  I have suggested the use of GCC for compiling the browser with its engine
+  v. 0.98 Alpha
+  Date- Friday, 6 December 2024(A special day)
+  
+ */
 #include <iostream>
 #include "headers/menu.h"
 #include "headers/config.h"
@@ -9,6 +19,7 @@
 void clearScreen() {
     std::cout << "\033[2J\033[1;1H";
 }
+
 
 // Function to get build information
 std::string getBuildInfo() {
@@ -31,7 +42,7 @@ std::string getOperatingSystem() {
 // Function to display the browser interface
 void displayInterface(const std::string& searchEngine) {
     clearScreen();
-    std::cout << "START\n";
+// START
     std::cout << "File   Edit   View   Buffers/Tabs   Tools   Settings   Help\n";
     std::cout << "\nPersiancat is currently a browser\n";
     std::cout << getBuildInfo() << "\n";
@@ -40,7 +51,7 @@ void displayInterface(const std::string& searchEngine) {
     std::cout << "\nURL- ............................................................................................... (GO)\n";
     std::cout << "Search Query- ................................................................................... (GO)\n";
     std::cout << "Search Engine- " << searchEngine << " (Change Search Engine)\n";
-    std::cout << "END\n";
+// END
 }
 
 // Main program loop
@@ -51,7 +62,7 @@ int main() {
     while (true) {
         displayInterface(searchEngine);
 
-        std::cout << "\nOptions:\n1. Open Menu (Press M)\n2. Enter URL (Press U)\n3. Search (Press S)\n4. Exit (Press E)\n";
+        std::cout << "\nOptions:\n1. Open Menu (Press M)\n2. Enter URL (Press U)\n3. Search (Press S)\n4. Exit (Press E). More Information(I). Copying(C)\n";
         char choice;
         std::cin >> choice;
 
@@ -71,6 +82,15 @@ int main() {
                 break;
             case 'E': // Exit
                 std::cout << "Exiting Persiancat. Goodbye!\n";
+                return 0;
+            case 'I': // Some Information
+                std::cout << "Project Persiancat, a suite of applications that is aimed to be minimal\n";
+                std::cout << "Project Persiancat v. 0.98-alpha| Released on 6 December 2024\n";
+                std::cout << "Project Persiancat is a free software, you are free to redistribute it under the certain conditions of the GNU GPL v3 and the GPL LGPL v3. For more details see the COPYING file or visit https://github.com/PranjalPrasad12/project-persiancat/blob/master/COPYING\n";
+                std::cout << "(C) Pranjal Prasad 2024. Thanks for using Persiancat"
+                return 0;
+            case 'C': // Display licensing and copying related stuff
+
                 return 0;
             default:
                 std::cout << "Invalid choice. Try again.\n";
